05aea093b6675f31b2484073b901d13d4348ed4f
Christian Stangerup <97446290+ChristianDWS@users.noreply.github.com>
2022-10-25 17:21:00 +0200

    Update PluginTemplate.java

--- src/main/java/org/mineacademy/template/PluginTemplate.java
+++ src/main/java/org/mineacademy/template/PluginTemplate.java
@@ -1,10 +1,14 @@
 package org.mineacademy.template;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.mineacademy.fo.plugin.SimplePlugin;
 import org.mineacademy.fo.remain.Remain;
 
@@ -16,13 +20,6 @@ import org.mineacademy.fo.remain.Remain;
  * It uses Foundation for fast and efficient development process.
  */
 public final class PluginTemplate extends SimplePlugin {
-
-	/**
-	 * LOL
-	 */
-	double apple = 1.50;
-	String yo = "Yo";
-
 
 	/**
 	 * Automatically perform login ONCE when the plugin starts.
@@ -43,15 +40,8 @@ public final class PluginTemplate extends SimplePlugin {
 	/* Events */
 	/* ------------------------------------------------------------------------------- */
 
-	/**
-	 * An example event that checks if the right clicked entity is a cow, and makes an explosion.
-	 * You can write your events to your main class without having to register a listener.
-	 *
-	 * @param event the click entity event, woo
-	 */
 	@EventHandler
 	public void onRightClick(PlayerInteractEntityEvent event) {
-
 		boolean isPig = event.getRightClicked().getType() == EntityType.PIG;
 		String message = isPig ? "That was a pig!" : "Not a pig!";
 
@@ -64,9 +54,27 @@ public final class PluginTemplate extends SimplePlugin {
 		}
 	}
 
-	// returns two numbers, multiplied
-	int multiply(int firstNumber, int secondNumber) {
-		return firstNumber * secondNumber;
-	}
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		PlayerInventory inventory = player.getInventory();
 
+		ItemStack[] contents = inventory.getContents();
+
+		for (int index = 0; index < contents.length; index++) {
+			ItemStack item = contents[index];
+			boolean isDiamond = item != null && item.getType() == Material.DIAMOND;
+
+			if (item == null || isDiamond) {
+				int diamondAmount = isDiamond ? item.getAmount() : 0;
+				contents[index] = new ItemStack(Material.DIAMOND, diamondAmount + 1);
+
+				player.sendMessage("You've been given 1x Diamond to your " + index + " slot.");
+
+				break;
+			}
+		}
+
+		inventory.setContents(contents);
+	}
 }
